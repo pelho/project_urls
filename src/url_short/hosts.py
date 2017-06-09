272362e6d34b7d168f4c51372751d55d8ec1ca50
pelho <pelho@yandex.ru>
2017-06-09 14:35:33 +0300

    Create hosts.py

--- src/url_short/hosts.py
+++ src/url_short/hosts.py
@@ -4,7 +4,7 @@ from django_hosts import patterns, host
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
     #host(r'live', settings.ROOT_URLCONF, name='live'),
-    host(r'(?!www).*', 'kirr.hostsconf.urls', name='wildcard'),
+    host(r'(?!www).*', 'url_short.hostsconf.urls', name='wildcard'),
 )
 
 '''
